e8d8ff231ecdf588c02a2f1b94fc6e3d5867832c
noah.tseng <t55619@hotmail.com.tw>
2022-05-30 07:50:41 +0800

    修正

--- src/main/java/com/noah/dao/AdminRepo.java
+++ src/main/java/com/noah/dao/AdminRepo.java
@@ -6,7 +6,7 @@ import org.springframework.data.repository.query.Param;
 
 import com.noah.entity.Admin;
 
-public interface AdminRepo extends JpaRepository<Admin,String>{
+public interface AdminRepo extends JpaRepository<Admin,Integer>{
 
 	@Query(value="select * from ADMIN where admin_name =:adminName and admin_pwd =:adminPwd" , nativeQuery = true)
 	public Admin checkNameAndPwd(@Param("adminName") String adminName, @Param("adminPwd") String adminPwd);
